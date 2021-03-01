0b29ce31064e79a58788a5f8124dda1705dcc1e3
Sourav <souravsker@gmail.com>
2021-03-01 13:13:45 -0330

    updated method for reverseTheString

--- string.js
+++ string.js
@@ -1,12 +1,15 @@
 // Question is : Write a function that reverse a string
 
 
-const inputString = "Hello there!"
-let outputString = "";
+const inputString = "I'm a student"
 
 
-for (let index = inputString.length -1; index >= 0; index--) {
-    outputString = outputString.concat(inputString[index]);
+const reverTheString = inputString => {
+    let outputString = "";
+    for (let index = inputString.length -1; index >= 0; index--) {
+        outputString = outputString.concat(inputString[index]);
+    }
+    return outputString;
 }
 
-console.log(outputString)
+console.log(reverTheString(inputString))
