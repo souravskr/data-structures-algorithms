cabe11c1b118b21ec4b34b00247f8e87600223d1
Sourav <souravsker@gmail.com>
2021-03-11 22:15:41 -0330

    improved operation time by reduced number of operation for move zero problem

--- moveZeros.js
+++ moveZeros.js
@@ -1,7 +1,7 @@
 const nums = [0,1,0,3,12];
 
 
-const moveZeros = (nums)=> {
+const moveZeros1 = (nums)=> {
     if (nums.length >= 1 || nums.length <= (10 ** 4)){
         nums.forEach(item => {
             if (item === 0 || item > 0x7FFFFFFF) {
@@ -15,6 +15,24 @@ const moveZeros = (nums)=> {
     return nums
 }
 
+const moveZeros = nums =>{
+    if (nums.length >= 1 || nums.length <= (10 ** 4)){
+        const arrayZero = []
+        let countZero = nums.indexOf(0);
+        while (countZero != -1) {
+            nums.splice(countZero, 1)
+            countZero = nums.indexOf(0)
+            arrayZero.push(0)
+        }
+        return nums.concat(arrayZero)
+    }
+
+    return nums
+    
+}
 
 
-console.log(moveZeros([0]))
+
+
+
+console.log(moveZeros(nums))
