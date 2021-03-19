2e23938951db31268a213b6e12a0f005ad24b332
Sourav <souravsker@gmail.com>
2021-03-19 20:50:40 -0230

    hash table from scratch solved

--- hashTable.js
+++ hashTable.js
@@ -1,17 +1,32 @@
 class HashTable {
     constructor(size){
       this.data = new Array(size);
+      this.keyValue = []
     }
 
     set(key, value){
-        const keyValue = []
-        keyValue.push(key)
-        keyValue.push(value)
-        this.data.push(keyValue)
+        // const keyValue = []
+        let keyIndex = this.data.flat().indexOf(key)
+        
+        if ( keyIndex === -1) {
+            this.keyValue.push(key)
+            this.keyValue.push(value)
+        } 
+        else{
+            
+            this.keyValue.splice(keyIndex + 1, 1, value)
+        }
+
+        // console.log(this.keyValue)
+        // console.log(keyIndex)
+        
+        return this.data.push(this.keyValue)
     }
 
     get(key){
-        
+        let flatArr = this.data.flat()
+        let keyIndex = flatArr.indexOf(key)
+        console.log(flatArr[keyIndex + 1])
     }
   
     _hash(key) {
@@ -25,6 +40,8 @@ class HashTable {
   
   const myHashTable = new HashTable(50);
   myHashTable.set('grapes', 10000)
+  myHashTable.get('grapes')
+  myHashTable.set('grapes', 100)
   myHashTable.get('grapes')
   myHashTable.set('apples', 9)
   myHashTable.get('apples')
