a74bd3525b7263e8c14e434650aa2280562b227d
Sourav <souravsker@gmail.com>
2021-03-28 23:13:08 -0230

    linked list

--- linkedlist.py
+++ linkedlist.py
@@ -1,4 +1,40 @@
-class Linkedlist:
-    def __init__(self, value, next):
-        self.value = value
-        self.next = next
+class Student:
+    def __init__(self, name, grade):
+        self.name = name
+        self.grade = grade
+
+    def get_grade(self):
+        return self.grade
+
+    def get_name(self):
+        return self.name
+
+
+class Course:
+    def __init__(self, name, max_students):
+        self.name = name
+        self.max_students = max_students
+        self.students = []
+
+    def add_student(self, student):
+        if len(self.students) < self.max_students:
+            return self.students.append(student)
+        return False
+
+    def avg_grade(self):
+        value = 0
+        for student in self.students:
+            value += student.get_grade()
+
+        return value/len(self.students)
+
+
+s1 = Student("Tim", 95)
+s2 = Student("Jack", 85)
+s3 = Student("John", 90)
+
+c1 = Course('Science', 3)
+c1.add_student(s1)
+c1.add_student(s3)
+
+print(c1.avg_grade())
