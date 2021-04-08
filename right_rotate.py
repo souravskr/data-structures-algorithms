d1c5cb9026e7dac684220e61d73e9904552d2fda
Sourav <souravsker@gmail.com>
2021-04-08 00:20:24 -0230

    rotate right list solved

--- right_rotate.py
+++ right_rotate.py
@@ -3,21 +3,40 @@ arr = [0, 1, 2]
 d = 4
 
 
+# def right_rotate(arr, d):
+#     size = len(arr)
+#     c_arr = [0] * (size)
+
+#     j = 2
+#     for _ in range(d):
+#         for i in range(1):
+#             c_arr[0] = arr[j]
+#             c_arr[i+1] = arr[i]
+#             c_arr[i+2] = arr[i+1]
+#         print(c_arr)
+#         arr = c_arr
+#         c_arr = [0] * (size)
+
+#     return arr
+
 def right_rotate(arr, d):
     size = len(arr)
+    print(f"Input: {arr}")
     c_arr = [0] * (size)
 
-    j = 2
+    k = size - 1
     for _ in range(d):
         for i in range(1):
-            c_arr[i] = arr[j]
-            c_arr[i+1] = arr[i]
-            c_arr[i+2] = arr[i+1]
-        print(c_arr)
+            c_arr[0] = arr[k]
+            for i in range(k):
+                c_arr[i+1] = arr[i]
+            # c_arr[i+2] = arr[i+1]
+        print(f"Rotate-{_+1}: {c_arr}")
         arr = c_arr
         c_arr = [0] * (size)
 
     return arr
 
 
-print(right_rotate(arr, d))
+(right_rotate(arr, d))
+# print(right_rotate(arr, d))
