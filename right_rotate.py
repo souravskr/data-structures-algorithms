6fa97d80c865bb4cf26b5f76f08a83defc7ec1b8
Sourav <souravsker@gmail.com>
2021-04-07 23:36:42 -0230

    right list problem--working

--- right_rotate.py
+++ right_rotate.py
@@ -1,25 +1,23 @@
 arr = [0, 1, 2]
+# arr = [1, 2, 3, 4, 5]
 d = 4
 
 
 def right_rotate(arr, d):
     size = len(arr)
-    c_arr = [0] * size
+    c_arr = [0] * (size)
 
-    i = 0
-    rotate_index = d + 1
-    while (rotate_index < size):
-        c_arr[i] = arr[rotate_index]
-        i += 1
-        rotate_index += 1
-    print(c_arr)
-    rotate_index = 0
-    while (rotate_index <= d):
-        c_arr[i] = arr[rotate_index]
-        i += 1
-        rotate_index += 1
+    j = 2
+    for _ in range(d):
+        for i in range(1):
+            c_arr[i] = arr[j]
+            c_arr[i+1] = arr[i]
+            c_arr[i+2] = arr[i+1]
+        print(c_arr)
+        arr = c_arr
+        c_arr = [0] * (size)
 
-    return c_arr
+    return arr
 
 
 print(right_rotate(arr, d))
