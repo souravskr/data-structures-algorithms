f4a62d01320aa6fd8d471bab2bd74e116d01c36f
Sourav <souravsker@gmail.com>
2021-04-11 01:04:45 -0230

    string manipulation

--- a.py
+++ a.py
@@ -1,8 +1,23 @@
-a = ["a", "b", "c"]
 
-s = ""
+a = [8, 5, 6, 16, 5]
+l = 1
+r = 3
 
-for i in a:
-    s += i
+size = len(a)
+b = []
 
-print(s)
+for i in range(size):
+    x1 = a[i] % (i + 1)
+    if x1 == 0:
+        x = a[i] // (i + 1)
+        if a[i] == (i + 1) * x:
+            if x >= l and x <= r:
+                b.append(True)
+            else:
+                b.append(False)
+
+    else:
+        b.append(False)
+
+
+print(b)
