f9f2dc02d1a3e009ac823998ec4f39e70d6d0eda
Sourav <souravsker@gmail.com>
2021-04-16 02:08:50 -0230

    insert into specific index

--- a.py
+++ a.py
@@ -1,9 +1,23 @@
-arr = [8, 5, 6, 16]
+arr = [5, 6, 8, 16]
 
 
 def insert_into(arr, num):
     arr = sorted(arr)
+    arr.append(0)
     size = len(arr)
-    i = 0
-    while (i < size):
-        if arr[i] > num:
+    for i in range(size):
+        if num > arr[size - 2]:
+            arr[size-1] = num
+            return arr
+        elif arr[i] > num:
+            for j in range(size - 1, i, -1):
+                temp = arr[j]
+                arr[j] = arr[j-1]
+                arr[j-1] = temp
+            arr[i] = num
+            break
+
+    return arr
+
+
+print(insert_into(arr, 15))
