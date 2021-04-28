9149451b23a83b1574f5e9730ab8e809a6e16811
Sourav <souravsker@gmail.com>
2021-04-28 00:40:27 -0230

    more filter practice

--- map.js
+++ map.js
@@ -21,3 +21,11 @@ console.log(mature)
 
 const retailCompanies = companies.filter(company => company.category === 'Retail')
 console.log(retailCompanies)
+
+const eightiesCompanies = companies.filter(company => company.start >= 1980 && company.start < 1990)
+
+console.log(eightiesCompanies)
+
+const lastedTenYears = companies.filter(company => (company.end - company.start) >= 10)
+
+console.log(lastedTenYears)
