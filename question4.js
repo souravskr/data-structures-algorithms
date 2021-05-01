781b0c7014de7667b410bcf5adc68bc8ca06a3e1
Sourav Sarker <souravsker@gmail.com>
2021-04-30 22:02:04 -0230

    new system

--- question4.js
+++ question4.js
@@ -3,7 +3,7 @@
 // ---> Inputs:  [0, 3, 4, 31], [4, 6, 30]
 // Output: [0, 3, 4, 6, 30, 31]
 
-// Selection Sort Algorithm
+// Selection Sort Algorithm 
 
 const mergeInputs = (arr1, arr2) => {
   const mergedArray = arr1.concat(arr2);
@@ -27,3 +27,5 @@ const arr1 = [0, 3, 4, 31];
 const arr2 = [4, 6, 30];
 
 console.log(mergeInputs(arr1, arr2));
+
+// Todo: 1. 
