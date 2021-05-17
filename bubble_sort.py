78ca08b2eb99c6bf5312e2b806ea76281eac9465
Sourav Sarker <souravsker@gmail.com>
2021-05-16 22:38:11 -0230

    bubble sort

---
+++ bubble_sort.py
@@ -0,0 +1,18 @@
+arr = [3, 1, 2, 5, 4]
+
+
+def bubble_sort(arr):
+    size = len(arr) - 1
+    sorted = False
+
+    while (not sorted):
+        sorted = True
+        for i in range(size):
+            if (arr[i] > arr[i+1]):
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+        size -= 1
+    return arr
+
+sorted_arr = bubble_sort(arr)
+
+print(sorted_arr)
