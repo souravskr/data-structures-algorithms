b573f45d7ead0d95e2345c6f317d06acd79dff62
sourav <souravsker@gmail.com>
2022-07-28 10:55:55 -0230

    O of log n with binary search

--- Udemy-Course/BigO/o-logn.py
+++ Udemy-Course/BigO/o-logn.py
@@ -1,20 +1,20 @@
-def binary_search(my_list, target):
-    size = len(my_list)
+def binary_search(arr, target):
+    size = len(arr)
     left_pointer = 0
     right_pointer = size - 1
 
     while left_pointer <= right_pointer:
-        mid_pointer = int((left_pointer + right_pointer) / 2)
+        mid = int((left_pointer + right_pointer) / 2)
 
-        if my_list[mid_pointer] == target:
-            print(f'GIVEN LIST: {my_list} \nTARGET: {target}')
-            return f'{my_list.index(target)}'
-        elif target > my_list[mid_pointer]:
-            left_pointer = mid_pointer + 1
+        if target == arr[mid]:
+            print(f'GIVEN LIST: {arr} \n TARGET: {target}')
+            return f'FOUND! Index of {target} is {arr.index(target)}'
+        elif target > mid:
+            left_pointer = mid + 1
         else:
-            right_pointer = mid_pointer - 1
+            right_pointer = mid - 1
 
 
 my_list = [i for i in range(1, 11)]
-target = 10
-print(binary_search(my_list, target))
+target_input = 1
+print(binary_search(arr=my_list, target=target_input))
