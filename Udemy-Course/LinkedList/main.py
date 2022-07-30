d15a249ad14abcd299ef6b0b0858cc02d8cb2be1
sourav <souravsker@gmail.com>
2022-07-29 22:10:53 -0230

    PoP Method for LinkedList

--- Udemy-Course/LinkedList/main.py
+++ Udemy-Course/LinkedList/main.py
@@ -87,6 +87,17 @@ class LinkedList:
         self.length += 1
         return True
 
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+        return temp
+
 
 my_linked_list = LinkedList(1)
 my_linked_list.append(2)
