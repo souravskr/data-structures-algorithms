67f5b451b243612701c096137357355f3b30cfd7
sourav <souravsker@gmail.com>
2022-07-31 10:44:44 -0230

    Doubly Linked List - Append Method For Doubly Linked List

--- Udemy-Course/Doubly LinkedList/main.py
+++ Udemy-Course/Doubly LinkedList/main.py
@@ -21,6 +21,19 @@ class DoublyLinkedList:
             print(temp.value)
             temp = temp.next
 
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
+
 
 my_doubly_linked_list = DoublyLinkedList(7)
+my_doubly_linked_list.append(4)
 my_doubly_linked_list.print_list()
