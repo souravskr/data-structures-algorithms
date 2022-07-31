89f91a7a9fd4ea56cd9e5111302f4aabe68b4a01
sourav <souravsker@gmail.com>
2022-07-31 12:29:54 -0230

    Doubly Linked List - `Prepend` Method For Doubly Linked List.

--- Udemy-Course/Doubly LinkedList/main.py
+++ Udemy-Course/Doubly LinkedList/main.py
@@ -47,11 +47,22 @@ class DoublyLinkedList:
         self.length -= 1
         return temp
 
-
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+        self.length += 1
+        return True
 
 
 my_doubly_linked_list = DoublyLinkedList(7)
 my_doubly_linked_list.append(4)
 my_doubly_linked_list.append(5)
 my_doubly_linked_list.pop()
+my_doubly_linked_list.prepend(1)
 my_doubly_linked_list.print_list()
