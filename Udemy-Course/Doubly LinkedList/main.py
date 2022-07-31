6fd00e2fffcdf06d5033553a38b798e7ec2b09b5
sourav <souravsker@gmail.com>
2022-07-31 12:48:12 -0230

    Doubly Linked List - `pop_first` Method For Doubly Linked List.

--- Udemy-Course/Doubly LinkedList/main.py
+++ Udemy-Course/Doubly LinkedList/main.py
@@ -59,10 +59,25 @@ class DoublyLinkedList:
         self.length += 1
         return True
 
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next
+            self.head.prev = None
+            temp.next = None
+        self.length -= 1
+        return temp
+
 
 my_doubly_linked_list = DoublyLinkedList(7)
 my_doubly_linked_list.append(4)
 my_doubly_linked_list.append(5)
 my_doubly_linked_list.pop()
 my_doubly_linked_list.prepend(1)
+my_doubly_linked_list.pop_first()
 my_doubly_linked_list.print_list()
