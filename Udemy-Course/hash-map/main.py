2b99ece39644610110d5b863c3e0d29588e1c9ff
sourav <souravsker@gmail.com>
2022-08-12 21:38:55 -0230

    HashTable class for HashMap

--- Udemy-Course/hash-map/main.py
+++ Udemy-Course/hash-map/main.py
@@ -1,6 +1,17 @@
-my_hash = [{
-    'key': 'value',
-    'key1': 'value1'
-}]
+class HashTable:
+    def __init__(self, size=7):
+        self.data_map = [None] * size
 
-print(my_hash)
+    def __hash(self, key):
+        my_hash = 0
+        for letter in key:
+            my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
+        return my_hash
+
+    def print_table(self):
+        for i, val in enumerate(self.data_map):
+            print(f'{i}: {val}')
+
+
+my_hash_table = HashTable()
+my_hash_table.print_table()
