8185dbca5692b653dbcfb12a046ec7b9ce53718a
Sourav Sarker <46949777+souravskr@users.noreply.github.com>
2022-08-14 22:33:22 -0230

    Time: 74 ms (58.29%), Space: 14.8 MB (6.45%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.py
@@ -0,0 +1,17 @@
+class Solution:
+    def searchInsert(self, arr: List[int], target: int) -> int:
+        if target not in arr:
+            arr.append(target)
+        arr.sort()
+        size = len(arr)
+        left = 0
+        right = size - 1
+
+        while left <= right:
+            mid = int((left + right)/2)
+            if target == arr[mid]:
+                return mid
+            elif target < arr[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
