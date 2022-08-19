a033adeb793dca38034270661de8c05bcdc88178
sourav <souravsker@gmail.com>
2022-08-19 12:14:42 -0230

    Graph - Remove Edge - Remove Vertex

--- Udemy-Course/Graphs/main.py
+++ Udemy-Course/Graphs/main.py
@@ -27,11 +27,27 @@ class Graph:
                 return True
         return False
 
+    def remove_vertex(self, v):
+        if v in self.add_list:
+            for vertex in self.add_list[v]:
+                self.add_list[vertex].remove(v)
+            del self.add_list[v]
+            return True
+        return False
+
 
 my_graph = Graph()
 my_graph.add_vertex('A')
 my_graph.add_vertex('B')
+my_graph.add_vertex('C')
+my_graph.add_vertex('D')
 my_graph.add_edge('A', 'B')
-my_graph.print_graph()
-my_graph.remove_edge('A', 'B')
+my_graph.add_edge('C', 'D')
+my_graph.add_edge('A', 'C')
+my_graph.add_edge('B', 'D')
+my_graph.add_edge('A', 'D')
+# my_graph.print_graph()
+# my_graph.remove_edge('A', 'B')
+# my_graph.print_graph()
+my_graph.remove_vertex('D')
 my_graph.print_graph()
