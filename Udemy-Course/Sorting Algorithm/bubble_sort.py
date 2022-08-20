8713c910022bd790aeeee4b345b918917cbbe2e3
sourav <souravsker@gmail.com>
2022-08-19 23:40:57 -0230

    Sorting Algo - Insertion Sort

--- Udemy-Course/Sorting Algorithm/bubble_sort.py
+++ Udemy-Course/Sorting Algorithm/bubble_sort.py
@@ -3,7 +3,7 @@ import time
 
 def bubble_sort(arr):
     size = len(arr)
-    for i in range(size -1, 0, -1):
+    for i in range(size - 1, 1, -1):
         for j in range(i):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
---
+++ Udemy-Course/Sorting Algorithm/insertion_sort.py
@@ -0,0 +1,17 @@
+import time
+
+
+def insertion_sort(arr):
+    size = len(arr)
+    for i in range(1, size):
+        temp = arr[i]
+        position = i - 1
+        while position >= 0 and arr[position] > temp:
+            arr[position + 1] = arr[position]
+            arr[position] = temp
+            position -= 1
+    return arr
+
+
+my_arr = [4, 2, 7, 1, 3]
+print(insertion_sort(my_arr))
