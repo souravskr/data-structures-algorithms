77a19aa305dacf36b2a7a63b4887d61906a0bcc2
Sourav Sarker <46949777+souravskr@users.noreply.github.com>
2022-08-21 12:45:49 -0230

    Time: 35 ms (91.48%), Space: 13.8 MB (59.15%) - LeetHub

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -3,10 +3,11 @@ class Solution:
         arr_string = [str(i) for i in arr]
         arr_string = int(''.join(arr_string))
         arr_string += 1
-        res = []
-        while arr_string:
-            right = arr_string % 10
-            res.append(right)
-            arr_string = arr_string // 10
-        res.reverse()
+        res = [int(n) for n in str(arr_string)]
+        # res = []
+        # while arr_string:
+        #     right = arr_string % 10
+        #     res.append(right)
+        #     arr_string = arr_string // 10
+        # res.reverse()
         return res
