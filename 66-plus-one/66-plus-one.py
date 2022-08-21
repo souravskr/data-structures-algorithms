a6e9c02ef15d41e84af02dd44e390cc1b7632e98
Sourav Sarker <46949777+souravskr@users.noreply.github.com>
2022-08-21 12:35:01 -0230

    Time: 61 ms (24.52%), Space: 13.8 MB (59.15%) - LeetHub

---
+++ 66-plus-one/66-plus-one.py
@@ -0,0 +1,12 @@
+class Solution:
+    def plusOne(self, arr: List[int]) -> List[int]:
+        arr_string = [str(i) for i in arr]
+        arr_string = int(''.join(arr_string))
+        arr_string += 1
+        res = []
+        while arr_string:
+            right = arr_string % 10
+            res.append(right)
+            arr_string = arr_string // 10
+        res.reverse()
+        return res
