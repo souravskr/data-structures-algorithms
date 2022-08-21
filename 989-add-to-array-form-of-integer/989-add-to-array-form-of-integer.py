859645785c0d4d615f93d302e30fba7e4639f61e
Sourav Sarker <46949777+souravskr@users.noreply.github.com>
2022-08-21 12:47:00 -0230

    Time: 323 ms (85.35%), Space: 14.9 MB (74.01%) - LeetHub

--- 989-add-to-array-form-of-integer/989-add-to-array-form-of-integer.py
+++ 989-add-to-array-form-of-integer/989-add-to-array-form-of-integer.py
@@ -2,10 +2,11 @@ class Solution:
     def addToArrayForm(self, num: List[int], k: int) -> List[int]:
         num = int(''.join([str(i) for i in num]))
         pre_res = num + k
-        res = []
-        while pre_res:
-            end_digit = pre_res % 10
-            res.append(end_digit)
-            pre_res = pre_res // 10
-        res.reverse()
+        res = [int(n) for n in str(pre_res)]
+        # res = []
+        # while pre_res:
+        #     end_digit = pre_res % 10
+        #     res.append(end_digit)
+        #     pre_res = pre_res // 10
+        # res.reverse()
         return res
