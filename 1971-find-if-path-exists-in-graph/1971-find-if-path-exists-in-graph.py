fce48fdf764e81ee8b29935655fa28ce4702d26e
Sourav Sarker <46949777+souravskr@users.noreply.github.com>
2022-08-27 00:59:29 -0230

    Time: 3012 ms (50.30%), Space: 309.4 MB (13.40%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,31 +1,64 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        def build_graph(edges):
-            adj_list = {}
-            for edge in edges:
-                a, b = edge
-                if a not in adj_list:
-                    adj_list[a] = []
-                if b not in adj_list:
-                    adj_list[b] = []
-                adj_list[a].append(b)
-                adj_list[b].append(a)
-            return adj_list
-
-
-        adj_list = build_graph(edges)
         
-        def dfs_undirected(graph, src, dst, visited):
+        '''
+            steps-->
+            1. make uni-directional adjacency list
+            2. run recursive dfs with visited variable --> every node we visit we mark visited
+            3. return bool whatever dfs returns
+            
+        '''
+        
+        adj_list = defaultdict(list)
+        for v1, v2 in edges:
+            adj_list[v1].append(v2)
+            adj_list[v2].append(v1)
+            
+        def dfs(src, dst, visited):
             if src == dst:
                 return True
             if src in visited:
                 return False
             visited.add(src)
-            for vertex in graph[src]:
-                if dfs_undirected(graph, vertex, dst, visited):
+            
+            for vertex in adj_list[src]:
+                if dfs(vertex, dst, visited):
                     return True
             return False
         
-        return dfs_undirected(adj_list, source, destination, set())
+        return dfs(source, destination, set())
+        
+        
+        
+        
+        
+        
+#         def build_graph(edges):
+#             adj_list = {}
+#             for edge in edges:
+#                 a, b = edge
+#                 if a not in adj_list:
+#                     adj_list[a] = []
+#                 if b not in adj_list:
+#                     adj_list[b] = []
+#                 adj_list[a].append(b)
+#                 adj_list[b].append(a)
+#             return adj_list
+
+
+#         adj_list = build_graph(edges)
+        
+#         def dfs_undirected(graph, src, dst, visited):
+#             if src == dst:
+#                 return True
+#             if src in visited:
+#                 return False
+#             visited.add(src)
+#             for vertex in graph[src]:
+#                 if dfs_undirected(graph, vertex, dst, visited):
+#                     return True
+#             return False
+        
+#         return dfs_undirected(adj_list, source, destination, set())
         
         
