958ac035d092288c20d918d62510d42bc59ca1fe
Sourav Sarker <46949777+souravskr@users.noreply.github.com>
2022-09-14 17:29:00 -0230

    Time: 86 ms (91.49%), Space: 16.3 MB (82.86%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.py
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.py
@@ -6,27 +6,44 @@
 #         self.right = right
 class Solution:
     def findTarget(self, root: Optional[TreeNode], target: int) -> bool:
-        result = []
-
-        def traverse(curr_node):
-            if curr_node.left:
-                traverse(curr_node.left)
-            result.append(curr_node.val)
-            if curr_node.right:
-                traverse(curr_node.right)
-
-        traverse(root)
-        
-        left = 0
-        right = len(result) - 1
-        
-        while left < right:
-            cur_sum = result[left] + result[right]
-            
-            if target > cur_sum:
-                left += 1
-            elif target < cur_sum:
-                right -= 1
+        if root is None:
+            return False
+        queue = collections.deque([root])
+        cur_res = set()
+        while queue:
+            cur_node = queue.popleft()
+            res_of_target = target - cur_node.val
+            if res_of_target not in cur_res:
+                cur_res.add(cur_node.val)
             else:
                 return True
+            if cur_node.left:
+                queue.append(cur_node.left)
+            if cur_node.right:
+                queue.append(cur_node.right)
         return False
+    
+#         result = []
+
+#         def traverse(curr_node):
+#             if curr_node.left:
+#                 traverse(curr_node.left)
+#             result.append(curr_node.val)
+#             if curr_node.right:
+#                 traverse(curr_node.right)
+
+#         traverse(root)
+        
+#         left = 0
+#         right = len(result) - 1
+        
+#         while left < right:
+#             cur_sum = result[left] + result[right]
+            
+#             if target > cur_sum:
+#                 left += 1
+#             elif target < cur_sum:
+#                 right -= 1
+#             else:
+#                 return True
+#         return False
