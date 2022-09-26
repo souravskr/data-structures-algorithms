1f5a313da4c1d13ac520538666064b3b2fd6aa1d
Sourav Sarker <46949777+souravskr@users.noreply.github.com>
2022-09-26 14:17:20 -0230

    Time: 89 ms (15.73%), Space: 14 MB (29.60%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.py
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.py
@@ -5,22 +5,39 @@
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        temp = head 
-        res_temp = []
         
-        while temp:
-            res_temp.append(temp.val)
-            temp = temp.next
+        dum = ListNode(0, head)
+        prev = dum
         
-        count = Counter(res_temp)
-        res_list = [k for k, v in count.items() if v == 1]
-        
-        dum = ListNode()
-        cur = dum
-        
-        for i in res_list:
-            cur.next = ListNode(i)
-            cur = cur.next
+        while head:
+            if head.next and head.val == head.next.val:
+                while head.next and head.val == head.next.val:
+                    head = head.next
+                prev.next = head.next
+                head = head.next
+            else:
+                head = head.next
+                prev = prev.next
         
         return dum.next
+        
+        # Naive Approach
+#         temp = head 
+#         res_temp = []
+        
+#         while temp:
+#             res_temp.append(temp.val)
+#             temp = temp.next
+        
+#         count = Counter(res_temp)
+#         res_list = [k for k, v in count.items() if v == 1]
+        
+#         dum = ListNode()
+#         cur = dum
+        
+#         for i in res_list:
+#             cur.next = ListNode(i)
+#             cur = cur.next
+        
+#         return dum.next
         
