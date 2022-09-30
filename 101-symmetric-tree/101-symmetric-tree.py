da37e95f6fa6693321ccc9f31bd1ade51669ca14
Sourav Sarker <46949777+souravskr@users.noreply.github.com>
2022-09-30 12:20:38 -0230

    Time: 55 ms (57.50%), Space: 13.9 MB (60.97%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.py
+++ 101-symmetric-tree/101-symmetric-tree.py
@@ -6,74 +6,21 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        # check if root is None then return True
         if root is None:
             return True
-        return self.isMirror(root.left, root.right)
         
-    def isMirror(self,t1, t2):
-        if not t1 or not t2:
-            return t1 == t2
-        
-        if t1.val != t2.val:
-            return False
-        
-        return self.isMirror(t1.left, t2.right) and self.isMirror(t1.right, t2.left)
-        
-        # queue = [root]
-#         res = []
-#         def traverse(cur):
-#             if cur.left is not None:
-#                 traverse(cur.left)
-#             res.append(cur.val)
-#             if cur.right is not None:
-#                 traverse(cur.right)
-#         traverse(root)
-        
-#         print(res)
-        
-#         size = len(res)
-#         if size % 2 != 0:
-#             mid = size // 2
-#             right = size -1
-#             for left in range(mid):
-#                 if res[left] != res[right]:
-#                     return False
-#                 right -= 1
-#             return True
-#         else:
-#             return False
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         while queue:
-#             cur = queue.pop(0)
-#             tree_arr.append(cur.val)
-#             if cur.left is not None:
-#                 queue.append(cur.left)
-#             if cur.right is not None:
-#                 queue.append(cur.right)
-        
-#         print(tree_arr)
-#         res_dict = Counter(tree_arr[1:])
-#         for k,v in res_dict.items():
-#             if v != 2:
-#                 return False
-#         return True
+        # Else check if left subtree and right subtree are mirrored
+        return isMirror(root.left, root.right)
+    
+def isMirror(tree1, tree2):
+    # Check if both are None return True, else return False
+    if not tree1 or not tree2:
+        return tree1 == tree2
+
+    # Return False if node values are not equal
+    if tree1.val != tree2.val:
+        return False
+
+    return isMirror(tree1.left, tree2.right) and isMirror(tree1.right, tree2.left)
+    
