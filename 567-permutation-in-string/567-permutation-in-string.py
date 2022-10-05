b7cda339e0a52affb6f44470e992693b37f951c8
Sourav Sarker <46949777+souravskr@users.noreply.github.com>
2022-10-05 14:27:22 -0230

    Time: 2945 ms (17.91%), Space: 13.8 MB (95.33%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,57 +1,35 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        
+        # def char_to_dict(s):
+        #     res = {}
+        #     for i in range(len(s)):
+        #         char_ord = ord(s[i]) - ord('a')
+        #         if char_ord in res:
+        #             res[char_ord] += 1
+        #         else:
+        #             res[char_ord] = 1
+        #     return res
+        
         s1_size = len(s1)
         s2_size = len(s2)
         if s1_size > s2_size:
             return False
-        s1 = ''.join(sorted(s1))
-
+        s1_dict = collections.Counter(s1)
         for i in range(s2_size - s1_size + 1):
-            new_str = ''.join(sorted(s2[i:i+s1_size]))
-            if s1 == new_str:
+            temp_dict = collections.Counter(s2[i:i+s1_size])
+            if s1_dict == temp_dict:
                 return True
         return False
-        
-#         def char_to_dict(s):
-#             res = {}
-#             for i in range(len(s)):
-#                 res[s[i]] = ord('a') - ord(s[i])
-#             return res
-        
+    
 #         s1_size = len(s1)
 #         s2_size = len(s2)
 #         if s1_size > s2_size:
 #             return False
-#         s1_dict = char_to_dict(s1)
+#         s1 = ''.join(sorted(s1))
+
 #         for i in range(s2_size - s1_size + 1):
-#             temp_dict = char_to_dict(s2[i:i+s1_size])
-#             if s1_dict == temp_dict:
+#             new_str = ''.join(sorted(s2[i:i+s1_size]))
+#             if s1 == new_str:
 #                 return True
 #         return False
-        
-        
-        
-        
-        
-#         def string_to_dict(s):
-#             s_dict = {}
-#             i = 1
-#             for c in s:
-#                 if c in s_dict:
-#                     s_dict[c] += 1
-#                 else:
-#                     s_dict[c] = i
-#             return s_dict
-
-#         if len(s1) > len(s2):
-#             return False
-#         s_dict = string_to_dict(s1)
-#         s1_size = len(s1)
-#         size = len(s2) - s1_size
-#         matched = False
-
-#         for i in range(size+1):
-#             if s_dict == string_to_dict(s2[i:s1_size + i]):
-#                 matched = True
-#         return matched
-    
