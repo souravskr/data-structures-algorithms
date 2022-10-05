0a9137d765b04528af5fd5c278ecadc21d359872
Sourav Sarker <46949777+souravskr@users.noreply.github.com>
2022-10-05 14:28:20 -0230

    Time: 1403 ms (23.89%), Space: 13.9 MB (95.33%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,16 +1,5 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        
-        # def char_to_dict(s):
-        #     res = {}
-        #     for i in range(len(s)):
-        #         char_ord = ord(s[i]) - ord('a')
-        #         if char_ord in res:
-        #             res[char_ord] += 1
-        #         else:
-        #             res[char_ord] = 1
-        #     return res
-        
         s1_size = len(s1)
         s2_size = len(s2)
         if s1_size > s2_size:
@@ -21,15 +10,3 @@ class Solution:
             if s1_dict == temp_dict:
                 return True
         return False
-    
-#         s1_size = len(s1)
-#         s2_size = len(s2)
-#         if s1_size > s2_size:
-#             return False
-#         s1 = ''.join(sorted(s1))
-
-#         for i in range(s2_size - s1_size + 1):
-#             new_str = ''.join(sorted(s2[i:i+s1_size]))
-#             if s1 == new_str:
-#                 return True
-#         return False
