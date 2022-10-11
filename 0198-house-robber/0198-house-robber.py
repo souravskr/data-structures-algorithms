67a8bfc49d930a04035d3b1631d0488d648ab87d
Sourav Sarker <46949777+souravskr@users.noreply.github.com>
2022-10-11 15:41:34 -0230

    Time: 47 ms (69.47%), Space: 13.8 MB (65.28%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,6 +1,8 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        p0, p1 = 0, 0
-        for m in nums:
-            p1, p0 = max(p0 + m, p1), p1
-        return p1
+        h1 = h2 = 0
+        for n in nums:
+            temp = max(h1 + n, h2)
+            h1 = h2
+            h2 = temp
+        return h2
