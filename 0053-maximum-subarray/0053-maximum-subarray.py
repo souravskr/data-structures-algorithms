7cc1620d2981ff14a18cf522e0ce792d05683ed7
Sourav Sarker <46949777+souravskr@users.noreply.github.com>
2022-10-13 13:45:55 -0230

    Time: 2464 ms (5.01%), Space: 27.8 MB (77.75%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,9 +1,17 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        c_sum = nums[0]
-        g_sum = nums[0]
+        #Kadane's Algorithm
+        res = nums[0]
         for i in range(1, len(nums)):
-            c_sum = max(nums[i], c_sum + nums[i])
-            if c_sum > g_sum:
-                g_sum = c_sum
-        return g_sum
+            nums[i] = max(nums[i], nums[i] + nums[i - 1])
+            res = max(res, nums[i])
+        return res
+    
+    
+        # c_sum = nums[0]
+        # g_sum = nums[0]
+        # for i in range(1, len(nums)):
+        #     c_sum = max(nums[i], c_sum + nums[i])
+        #     if c_sum > g_sum:
+        #         g_sum = c_sum
+        # return g_sum
