42de662eef5a75b40ad58885762ebdd7eacecee8
Sourav Sarker <46949777+souravskr@users.noreply.github.com>
2022-10-18 11:51:04 -0230

    Time: 99 ms (47.86%), Space: 14.3 MB (17.92%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,12 +1,26 @@
 class Solution:
     def canConstruct(self, str1: str, str2: str) -> bool:
+        str1_dict = Counter(str1)
+        str2_dict = Counter(str2)
         res = ''
-        copy_str = list(str2)
         for c in str1:
-            if c in copy_str:
+            if str1_dict[c] <= str2_dict[c]:
                 res += c
-                index = copy_str.index(c)
-                copy_str.pop(index)
+                str1_dict[c] -= 1
+                str2_dict[c] -= 1
         if res == str1:
             return True
         return False
+    
+    
+    
+        # res = ''
+        # copy_str = list(str2)
+        # for c in str1:
+        #     if c in copy_str:
+        #         res += c
+        #         index = copy_str.index(c)
+        #         copy_str.pop(index)
+        # if res == str1:
+        #     return True
+        # return False
