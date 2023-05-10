7941fd296470b1ac79a8aa6d9a9b6c42cdaf69dd
Sourav Sarker <souravsker@gmail.com>
2023-05-09 22:56:39 -0230

    Receive messages between the channels.

--- status-checker/main.go
+++ status-checker/main.go
@@ -5,22 +5,29 @@ import (
 	"net/http"
 )
 
-func checkStatus(link string) {
+func checkStatus(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Printf("%s 🌎 is down!\n", link)
+		c <- "Down"
 		return
 	}
 	fmt.Printf("%s 🌎 is up!\n", link)
+	c <- "Up"
 }
 func main() {
 	urls := []string{
 		"https://google.com",
 		"https://facebook.com",
 		"https://golang.org",
-		"https://sequencebio.com",
+		"https://amazon.com",
 	}
+	c := make(chan string)
+
 	for _, url := range urls {
-		checkStatus(url)
+		go checkStatus(url, c)
+	}
+	for i := 0; i < len(urls); i++ {
+		fmt.Println(<-c)
 	}
 }
